732c82d20e709d99501a93fb81a5eeea2b64a9e5
Elena freire <elenafreire75@gmail.com>
2024-05-26 10:47:38 +0000

    added an exit button and change const to var

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,50 @@
 // Quiz Data
-const quizData = [
-    {question: 'What do you call ordinary people in the wizarding world?', choices: ['Peeps', 'Folks', 'Muggle', 'Mordor'], answer: 'Muggle'},
-    {question: 'What do the students call the Gryffindor ghost?', choices: ['Nagini', 'Moaning Myrtle', 'The Fat Lady', 'Nearly Headless Nick'], answer: 'Nearly Headless Nick'},
-    {question: 'What is the main ingredient in Harry Potter wand?', choices: ['A phoenix feather', 'A rabbit foot', 'A Newt Tail', 'A basilik tooth'], answer: 'A phoenix feather'},
-    {question: 'Where in Kings Cross Station does the Hogwarts Express stop?', choices: ['Platform 9 and 1/2', 'Platform 9 and 3/4', 'Platform 10 and 3/4', 'Platform 9 and 1/6'], answer: 'Platform 9 and 3/4'},
-    {question: 'Who is Tom Marvolo Riddle?', choices: ['Albus Dumbledore', 'Severus Snape', 'Lord Voldemort', 'Remus Lupin'], answer: 'Lord Voldemort'},
-    {question: 'Who does Hermione Granger go with to the Yule Ball?', choices: ['Draco Malfoy', 'Ron Weasley', 'Harry Potter', 'Viktor Krum'], answer: 'Viktor Krum'},
-    {question: 'What is the Name of Lord Voldemort\'s snake?', choices: ['Nagini', 'Basilisk', 'Hedwig', 'Mrs. Norris'], answer: 'Nagini'},
-    {question: 'What position does Harry play on the Gryffindor Quidditch team?', choices: ['Seeker', 'Goalkeeper', 'Captain', 'Forward'], answer: 'Seeker'},
-    {question: 'What is the name of the magical map that shows the entire layout of Hogwarts?', choices: ['The Leaky Cauldron', 'The Marauder\'s Map', 'The Map', 'Hogwarts Map'], answer: 'The Marauder\'s Map'},
-    {question: 'What is the name of the magical creature that can only be seen by those who have witnessed death?', choices: ['Unicorn', 'Fluffy', 'Ukrainian Ironbelly', 'Thestral'], answer: 'Thestral'}
+var quizData = [
+    { question: 'What do you call ordinary people in the wizarding world?', choices: ['Peeps', 'Folks', 'Muggle', 'Mordor'], answer: 'Muggle' },
+    { question: 'What do the students call the Gryffindor ghost?', choices: ['Nagini', 'Moaning Myrtle', 'The Fat Lady', 'Nearly Headless Nick'], answer: 'Nearly Headless Nick' },
+    { question: 'What is the main ingredient in Harry Potter wand?', choices: ['A phoenix feather', 'A rabbit foot', 'A Newt Tail', 'A basilik tooth'], answer: 'A phoenix feather' },
+    { question: 'Where in Kings Cross Station does the Hogwarts Express stop?', choices: ['Platform 9 and 1/2', 'Platform 9 and 3/4', 'Platform 10 and 3/4', 'Platform 9 and 1/6'], answer: 'Platform 9 and 3/4' },
+    { question: 'Who is Tom Marvolo Riddle?', choices: ['Albus Dumbledore', 'Severus Snape', 'Lord Voldemort', 'Remus Lupin'], answer: 'Lord Voldemort' },
+    { question: 'Who does Hermione Granger go with to the Yule Ball?', choices: ['Draco Malfoy', 'Ron Weasley', 'Harry Potter', 'Viktor Krum'], answer: 'Viktor Krum' },
+    { question: 'What is the Name of Lord Voldemort\'s snake?', choices: ['Nagini', 'Basilisk', 'Hedwig', 'Mrs. Norris'], answer: 'Nagini' },
+    { question: 'What position does Harry play on the Gryffindor Quidditch team?', choices: ['Seeker', 'Goalkeeper', 'Captain', 'Forward'], answer: 'Seeker' },
+    { question: 'What is the name of the magical map that shows the entire layout of Hogwarts?', choices: ['The Leaky Cauldron', 'The Marauder\'s Map', 'The Map', 'Hogwarts Map'], answer: 'The Marauder\'s Map' },
+    { question: 'What is the name of the magical creature that can only be seen by those who have witnessed death?', choices: ['Unicorn', 'Fluffy', 'Ukrainian Ironbelly', 'Thestral'], answer: 'Thestral' }
 ];
 
 // HTML Element References
-const introductionSection = document.getElementById('introduction');
-const letTheGameBeginButton = document.getElementById('letTheGameBegin');
-const quizContainer = document.getElementById('quiz');
-const resultContainer = document.getElementById('result');
-const submitButton = document.getElementById('submit');
-const retryButton = document.getElementById('retry');
-const showAnswerButton = document.getElementById('showAnswer');
+var introductionSection = document.getElementById('introduction');
+var letTheGameBeginButton = document.getElementById('letTheGameBegin');
+var quizContainer = document.getElementById('quiz');
+var resultContainer = document.getElementById('result');
+var submitButton = document.getElementById('submit');
+var retryButton = document.getElementById('retry');
+var showAnswerButton = document.getElementById('showAnswer');
+var exitButton = document.getElementById('exit');
 
-let currentQuestion = 0;
-let score = 0;
-let incorrectAnswers = [];
+var currentQuestion = 0;
+var score = 0;
+var incorrectAnswers = [];
 
 // Event Listeners
 letTheGameBeginButton.addEventListener('click', startGame);
 submitButton.addEventListener('click', checkAnswer);
 retryButton.addEventListener('click', retryQuiz);
 showAnswerButton.addEventListener('click', showAnswer);
+exitButton.addEventListener('click', exitQuiz);
 
-// Functions
+// Function to start the game
 function startGame() {
     introductionSection.style.display = 'none';
     quizContainer.style.display = 'block';
+    submitButton.style.display = 'inline-block';
     displayQuestion();
-}
 
+    submitButton.addEventListener('click', checkAnswer);
+    retryButton.addEventListener('click', retryQuiz);
+    showAnswerButton.addEventListener('click', showAnswer);
+    exitButton.addEventListener('click', exitQuiz);
+}
 
 // Function to display questions
 function displayQuestion() {
@@ -63,6 +70,7 @@ function displayQuestion() {
 
         const optionTextNode = document.createTextNode(optionText);
 
+        option.appendChild(radio);
         option.appendChild(optionTextNode);
         optionsElement.appendChild(option);
     });
@@ -101,10 +109,40 @@ function checkAnswer() {
 
 // Function to display results
 function displayResult() {
-    quizContainer.classList.add('hide');
-    submitButton.classList.add('hide');
-    retryButton.classList.remove('hide');
-    showAnswerButton.classList.remove('hide');
+    quizContainer.style.display = 'none';
+    submitButton.style.display = 'none';
+    retryButton.style.display = 'inline-block';
+    showAnswerButton.style.display = 'inline-block';
+    exitButton.style.display = 'inline-block';
+
+    resultContainer.innerHTML = `
+        <p>You scored ${score} out of ${quizData.length}!</p>
+    `;
+
+    resultContainer.style.display = 'block';
+}
+
+// Function to retry quiz
+function retryQuiz() {
+    currentQuestion = 0;
+    score = 0;
+    incorrectAnswers = [];
+    quizContainer.style.display = 'block';
+    submitButton.style.display = 'inline-block';
+    retryButton.style.display = 'none';
+    showAnswerButton.style.display = 'none';
+    exitButton.style.display = 'none';
+    resultContainer.style.display = 'none';
+    displayQuestion();
+}
+
+// Function to show answers
+function showAnswer() {
+    quizContainer.style.display = 'none';
+    submitButton.style.display = 'none';
+    retryButton.style.display = 'inline-block';
+    showAnswerButton.style.display = 'none';
+    exitButton.style.display = 'inline-block';
 
     let incorrectAnswersHtml = '';
     for (let i = 0; i < incorrectAnswers.length; i++) {
@@ -122,22 +160,11 @@ function displayResult() {
         <p>Incorrect Answers:</p>
         ${incorrectAnswersHtml}
     `;
+
+    resultContainer.style.display = 'block';
 }
 
-// Function to retry quiz
-function retryQuiz() {
-    currentQuestion = 0;
-    score = 0;
-    incorrectAnswers = [];
-    quizContainer.classList.remove('hide');
-    submitButton.classList.remove('hide');
-    retryButton.classList.add('hide');
-    showAnswerButton.classList.add('hide');
-    resultContainer.innerHTML = '';
-    displayQuestion();
-}
-
-// Function to show answers
-function showAnswer() {
-    displayResult();
+// Function to exit quiz
+function exitQuiz() {
+    window.location.reload(); // Reload the page to start fresh
 }
