dcbaf08da73e39d2c316c6948e377bb857d519c6
Elena freire <elenafreire75@gmail.com>
2024-05-26 03:37:11 +0000

    change the code format for js.script and index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,70 +1,15 @@
 // Quiz Data
 const quizData = [
-    {
-        question: 'What do you call ordinary people in the wizarding world?',
-        options: ['Peeps', 'Folks', 'Muggle', 'Mordor'],
-        answer: 'Muggle',
-    },
-    {
-        question: 'What do the students call the Gryffindor ghost? ',
-        options: ['Nagini', 'Moaning Myrtle', 'The Fat Lady', 'Nearly Headless Nick'],
-        answer: 'Nearly Headless Nick',
-    },
-    {
-        question: 'What is the main ingredient in Harry Potter wand??',
-        options: ['A phoenix feather', 'A rabbit foot', 'A Newt Tail', 'A basilik tooth'],
-        answer: 'A phoenix feather',
-    },
-    {
-        question: 'Where in Kings Cross Station does the Hogwarts Express stop? ',
-        options: ['Platform 9 and 1/2', 'Platform 9 and 3/4', 'Platform 10 and 3/4', 'Platform 9 and 1/6'],
-        answer: 'Platform 9 and 3/4',
-    },
-    {
-        question: 'Who is Tom Marvolo Riddle?',
-        options: [
-            'Albus Dumbledore',
-            'Severus Snape',
-            'Lord Voldemort',
-            'Remus Lumpin',
-        ],
-        answer: 'Lord Voldemort',
-    },
-    {
-        question: 'who does Hermione Granger go with to the Yule Ball? ',
-        options: ['Draco Malfoy', 'Ron Weasley', 'Harry Potter', 'Viktor Krum'],
-        answer: 'Viktor Krum',
-    },
-    {
-        question: 'What is the Name of Lord Voldemort snake?',
-        options: [
-            'Nagini',
-            'Basilik',
-            'Hedwig',
-            'Mrs. Morris',
-        ],
-        answer: 'Nagini',
-    },
-    {
-        question: 'What position does Harry play on the Gryffindor Quidditch team?',
-        options: ['seeker', 'goal', 'captain', 'forward'],
-        answer: 'seeker',
-    },
-    {
-        question: 'What is the name of the magical map that shows the entire layout of Hogwarts?',
-        options: [
-            'The Leaky Cauldron',
-            'The Marauders',
-            'The Map',
-            'Hogwarts Map',
-        ],
-        answer: 'The Marauders',
-    },
-    {
-        question: 'What is the name of the magical creature that can only be seen by those who have witnessed death?',
-        options: ['Unicorn', 'Fluffy', 'Ukrainian Ironbelly ', 'Thestral'],
-        answer: 'Thestral',
-    },
+    {question: 'What do you call ordinary people in the wizarding world?', choices: ['Peeps', 'Folks', 'Muggle', 'Mordor'], answer: 'Muggle'},
+    {question: 'What do the students call the Gryffindor ghost?', choices: ['Nagini', 'Moaning Myrtle', 'The Fat Lady', 'Nearly Headless Nick'], answer: 'Nearly Headless Nick'},
+    {question: 'What is the main ingredient in Harry Potter wand?', choices: ['A phoenix feather', 'A rabbit foot', 'A Newt Tail', 'A basilik tooth'], answer: 'A phoenix feather'},
+    {question: 'Where in Kings Cross Station does the Hogwarts Express stop?', choices: ['Platform 9 and 1/2', 'Platform 9 and 3/4', 'Platform 10 and 3/4', 'Platform 9 and 1/6'], answer: 'Platform 9 and 3/4'},
+    {question: 'Who is Tom Marvolo Riddle?', choices: ['Albus Dumbledore', 'Severus Snape', 'Lord Voldemort', 'Remus Lupin'], answer: 'Lord Voldemort'},
+    {question: 'Who does Hermione Granger go with to the Yule Ball?', choices: ['Draco Malfoy', 'Ron Weasley', 'Harry Potter', 'Viktor Krum'], answer: 'Viktor Krum'},
+    {question: 'What is the Name of Lord Voldemort\'s snake?', choices: ['Nagini', 'Basilisk', 'Hedwig', 'Mrs. Norris'], answer: 'Nagini'},
+    {question: 'What position does Harry play on the Gryffindor Quidditch team?', choices: ['Seeker', 'Goalkeeper', 'Captain', 'Forward'], answer: 'Seeker'},
+    {question: 'What is the name of the magical map that shows the entire layout of Hogwarts?', choices: ['The Leaky Cauldron', 'The Marauder\'s Map', 'The Map', 'Hogwarts Map'], answer: 'The Marauder\'s Map'},
+    {question: 'What is the name of the magical creature that can only be seen by those who have witnessed death?', choices: ['Unicorn', 'Fluffy', 'Ukrainian Ironbelly', 'Thestral'], answer: 'Thestral'}
 ];
 
 // HTML Element References
@@ -75,7 +20,6 @@ const resultContainer = document.getElementById('result');
 const submitButton = document.getElementById('submit');
 const retryButton = document.getElementById('retry');
 const showAnswerButton = document.getElementById('showAnswer');
-
 
 let currentQuestion = 0;
 let score = 0;
@@ -94,6 +38,7 @@ function startGame() {
     displayQuestion();
 }
 
+
 // Function to display questions
 function displayQuestion() {
     const questionData = quizData[currentQuestion];
@@ -105,7 +50,7 @@ function displayQuestion() {
     const optionsElement = document.createElement('div');
     optionsElement.className = 'options';
 
-    const shuffledOptions = [...questionData.options].sort(() => Math.random() - 0.5);
+    const shuffledOptions = [...questionData.choices].sort(() => Math.random() - 0.5);
 
     shuffledOptions.forEach(optionText => {
         const option = document.createElement('label');
@@ -118,7 +63,6 @@ function displayQuestion() {
 
         const optionTextNode = document.createTextNode(optionText);
 
-        option.appendChild(radio);
         option.appendChild(optionTextNode);
         optionsElement.appendChild(option);
     });
@@ -126,8 +70,6 @@ function displayQuestion() {
     quizContainer.innerHTML = '';
     quizContainer.appendChild(questionElement);
     quizContainer.appendChild(optionsElement);
-
-    submitButton.style.display = 'inline-block';
 }
 
 // Function to check answers
@@ -157,50 +99,45 @@ function checkAnswer() {
     }
 }
 
-
-//function to display results
+// Function to display results
 function displayResult() {
-    quizContainer.style.display = 'none';
-    submitButton.style.display = 'none';
-    retryButton.style.display = 'inline-block';
-    showAnswerButton.style.display = 'inline-block';
-    resultContainer.innerHTML = `You scored ${score} out of ${quizData.length}!`;
+    quizContainer.classList.add('hide');
+    submitButton.classList.add('hide');
+    retryButton.classList.remove('hide');
+    showAnswerButton.classList.remove('hide');
+
+    let incorrectAnswersHtml = '';
+    for (let i = 0; i < incorrectAnswers.length; i++) {
+        incorrectAnswersHtml += `
+            <p>
+                <strong>Question:</strong> ${incorrectAnswers[i].question}<br>
+                <strong>Your Answer:</strong> ${incorrectAnswers[i].incorrectAnswer}<br>
+                <strong>Correct Answer:</strong> ${incorrectAnswers[i].correctAnswer}
+            </p>
+        `;
+    }
+
+    resultContainer.innerHTML = `
+        <p>You scored ${score} out of ${quizData.length}!</p>
+        <p>Incorrect Answers:</p>
+        ${incorrectAnswersHtml}
+    `;
 }
 
-//Function to retry quiz
+// Function to retry quiz
 function retryQuiz() {
     currentQuestion = 0;
     score = 0;
     incorrectAnswers = [];
-    quizContainer.style.display = 'block';
-    submitButton.style.display = 'inline-block';
-    retryButton.style.display = 'none';
-    showAnswerButton.style.display = 'none';
+    quizContainer.classList.remove('hide');
+    submitButton.classList.remove('hide');
+    retryButton.classList.add('hide');
+    showAnswerButton.classList.add('hide');
     resultContainer.innerHTML = '';
     displayQuestion();
 }
 
 // Function to show answers
 function showAnswer() {
-    quizContainer.style.display = 'none';
-    submitButton.style.display = 'none';
-    retryButton.style.display = 'inline-block';
-    showAnswerButton.style.display = 'none';
-
-    let incorrectAnswersHtml = '';
-    for (let i = 0; i < incorrectAnswers.length; i++) {
-        incorrectAnswersHtml += `
-      <p>
-        <strong>Question:</strong> ${incorrectAnswers[i].question}<br>
-        <strong>Your Answer:</strong> ${incorrectAnswers[i].incorrectAnswer}<br>
-        <strong>Correct Answer:</strong> ${incorrectAnswers[i].correctAnswer}
-      </p>
-    `;
-    }
-
-    resultContainer.innerHTML = `
-    <p>You scored ${score} out of ${quizData.length}!</p>
-    <p>Incorrect Answers:</p>
-    ${incorrectAnswersHtml}
-  `;
+    displayResult();
 }
